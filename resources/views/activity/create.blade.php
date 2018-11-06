a694413acc1cbe46ea0c822b6a54c8a8a20bddc5
ronnievm <ronnievm20@outlook.com>
2018-11-06 11:24:54 +0100

    Fixed activity creation page

--- resources/views/activity/create.blade.php
+++ resources/views/activity/create.blade.php
@@ -3,4 +3,22 @@
 
 @section('content')
     <p>test</p>
+
+    <div class="row">
+        <div class="col-xs-12">
+            <form method="post" action="{{ route('register') }}" class="form-horizontal">
+                {!! csrf_field() !!}
+
+                <div class="form-group {{ $errors->has('first_name') ? 'has-error' : ''}}">
+                    <label for="first_name" class="control-label col-sm-4">Voornaam *</label>
+                    <div class="col-sm-8">
+                        <input type="text" name="first_name" id="first_name" value="{{ old('first_name') }}" class="form-control">
+                        {!! $errors->first('first_name', '<p class="help-block">:message</p>') !!}
+                    </div>
+                </div>
+
+
+            </form>
+        </div>
+    </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -44,7 +44,7 @@
                 </div>
 
                 <div class="form-group {{ $errors->has('phone_number') ? 'has-error' : ''}}">
-                    <label for="phone_number" class="control-label col-sm-4">Telefoonnummer</label>
+                    <label for="phone_number" class="control-label col-sm-4">Telefoonnummer *</label>
                     <div class="col-sm-8">
                         <input type="text" name="phone_number" id="phone_number" value="{{ old('phone_number') }}" class="form-control">
                         {!! $errors->first('phone_number', '<p class="help-block">:message</p>') !!}
