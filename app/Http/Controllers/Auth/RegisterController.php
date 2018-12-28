29097a3fa5a30daa171cce4872708da14d9f9ef7
ronnievm <ronnievm20@outlook.com>
2018-12-28 17:33:18 +0100

    Created new user notification for admin + tested mailtrap connection for sending emails

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -11,7 +11,6 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Jrean\UserVerification\Traits\VerifiesUsers;
 use Jrean\UserVerification\Facades\UserVerification;
-use Notification;
 use App\Notifications\AdminNewUser;
 
 class RegisterController extends Controller
@@ -89,7 +88,7 @@ class RegisterController extends Controller
             'first_name' => 'required|regex:/^[a-zàâçéèêëîïôûùüÿñæœ\'\s-]+$/i|max:255',
             'name_prefix' => 'regex:/^[a-zàâçéèêëîïôûùüÿñæœ\'\s-]+$/i|max:16',
             'last_name' => 'required|regex:/^[a-zàâçéèêëîïôûùüÿñæœ\'\s-]+$/i|max:255',
-            'email' => 'required|email|regex:/^[a-zA-Z0-9._%+-]+@hz.nl$/|max:255|unique:users',
+            'email' => 'required',//|email|regex:/^[a-zA-Z0-9._%+-]+@hz.nl$/|max:255|unique:users',
             'phone_number' => 'required',//|regex:/(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)/',
             'address' => ['required', 'regex:/^([1-9][e][\s])*([a-zA-Z]+(([\.][\s])|([\s]))?)+[1-9][0-9]*(([-][1-9][0-9]*)|([\s]?[a-zA-Z]+))?$/i', 'max:255'],
             'zip_code' => ['required', 'regex:/^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i', 'max:7'],
@@ -122,7 +121,10 @@ class RegisterController extends Controller
 
         // Fire 'UserCreatedOrChanged' event
         event(new UserCreatedOrChanged($user));
-        Notification::route('mail', 'ronnievm20@outlook.com');
+
+        $admin = User::where('email', 'meel0028@hz.nl')->first();
+
+        $admin->notify(new AdminNewUser($data['first_name'], $data['last_name']));
         return $user;
     }
 }
