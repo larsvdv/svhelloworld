73169a1607fcc8bedcd44a26b9a82237ead4025e
ronnievm <ronnievm20@outlook.com>
2019-01-31 11:33:52 +0100

    Fixed user signup regex to include periods in the address

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -90,7 +90,7 @@ class RegisterController extends Controller
             'last_name' => 'required|regex:/^[a-zàâçéèêëîïôûùüÿñæœ\'\s-]+$/i|max:255',
             'email' => 'required|email|regex:/^[a-zA-Z0-9._%+-]+@hz.nl$/|max:255|unique:users',
             'phone_number' => ['required', 'regex:/(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)/'],
-            'address' => ['required', 'regex:/^([1-9][e][\s])*([a-zA-Z]+(([\.][\s])|([\s]))?)+[1-9][0-9]*(([-][1-9][0-9]*)|([\s]?[a-zA-Z]+))?$/i', 'max:255'],
+            'address' => ['required', 'regex:/^([1-9][e][\s])*([a-zA-Z]+(([\.][\s]?)|([\s]))?)+[1-9][0-9]*(([-][1-9][0-9]*)|([\s]?[a-zA-Z]+))?$/i', 'max:255'],
             'zip_code' => ['required', 'regex:/^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i', 'max:7'],
             'city' => ['required', 'regex:/^([a-zA-Z\x{0080}-\x{024F}]+(?:. |-| |\'))*[a-zA-Z\x{0080}-\x{024F}]*$/u', 'max:255'],
             'password' => 'required|min:8|confirmed',
