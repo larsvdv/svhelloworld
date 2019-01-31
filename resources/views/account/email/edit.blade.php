19086f2af030721c302144aa368516c1dcc8497a
ronnievm <ronnievm20@outlook.com>
2019-01-31 14:02:15 +0100

    Updated forms to be positioned to the left more instead of centered

--- resources/views/account/email/edit.blade.php
+++ resources/views/account/email/edit.blade.php
@@ -10,23 +10,23 @@
                 {!! csrf_field() !!}
 
                 <div class="form-group {{ $errors->has('email') ? 'has-error' : ''}}">
-                    <label for="email" class="control-label col-sm-4">Nieuw e-mailadres</label>
-                    <div class="col-sm-8">
+                    <label for="email" class="control-label col-sm-2">Nieuw e-mailadres</label>
+                    <div class="col-sm-6">
                         <input type="email" name="email" id="email" value="{{ old('email') }}" class="form-control">
                         {!! $errors->first('email', '<p class="help-block">:message</p>') !!}
                     </div>
                 </div>
 
                 <div class="form-group {{ $errors->has('email_confirmation') ? 'has-error' : ''}}">
-                    <label for="email" class="control-label col-sm-4">Herhaal e-mailadres</label>
-                    <div class="col-sm-8">
+                    <label for="email" class="control-label col-sm-2">Herhaal e-mailadres</label>
+                    <div class="col-sm-6">
                         <input type="email" name="email_confirmation" id="email_confirmation" class="form-control">
                         {!! $errors->first('email_confirmation', '<p class="help-block">:message</p>') !!}
                     </div>
                 </div>
 
                 <div class="form-group">
-                    <div class="col-sm-offset-4 col-sm-8">
+                    <div class="col-sm-offset-2 col-sm-8">
                         <a href="{{ route('account.index') }}" class="btn btn-danger">Annuleren</a>
                         <button type="submit" class="btn btn-primary">E-mailadres wijzigen</button>
                     </div>
