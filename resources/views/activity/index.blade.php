596a699116567386f9584b09fee746cc7ecef2f7
cassis163 <cas.aangeenbrug@gmail.com>
2020-02-07 12:31:38 +0100

    Add upcoming activities table in activity/index.blade.php

--- resources/views/activity/index.blade.php
+++ resources/views/activity/index.blade.php
@@ -2,50 +2,13 @@
 @section('title', 'Aanmelden activiteiten')
 
 @section('content')
-    <h3>Activiteiten</h3>
-    @if ($activities->count())
+    @if ($availableActivities->count() + $upcomingActivities->count())
         <p>Dit is een overzicht van de activiteiten die georganiseerd worden door Studievereniging "Hello World".</p>
 
-        <!-- Available activities -->
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped table-hover">
-                <thead>
-                <tr>
-                    <th>Naam</th>
-                    <th>Datum en tijd</th>
-                    <th>Aanmeldperiode</th>
-                    <th>ITP-waarde</th>
-                    <th>Acties</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($availableActivities as $activity)
-                    <tr>
-                        <td>
-                            <a href="{{ route('activity.show', $activity->id) }}">{{ $activity->title }}</a>
-                        </td>
-                        <td>
-                            @datetime($activity->starts_at) t/m @datetime($activity->ends_at)
-                        </td>
-                        <td>
-                            @date($activity->available_from) t/m @date($activity->available_to)
-                        </td>
-                        <td>
-                            @if(isset($activity->itp_value))
-                                {{ $activity->itp_value }}
-                            @else
-                                Onbekend
-                            @endif
-                        </td>
-                        <td>
-                            <a href="{{ route('activity.show', $activity->id) }}" class="btn btn-primary btn-xs">Bekijken</a>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+        @if ($availableActivities->count())
+            <h3>Beschikbare activiteiten</h3>
 
-            <!-- Unavailable activities -->
+            <!-- Available activities -->
             <div class="table-responsive">
                 <table class="table table-bordered table-striped table-hover">
                     <thead>
@@ -58,7 +21,7 @@
                     </tr>
                     </thead>
                     <tbody>
-                    @foreach($unavailableActivities as $activity)
+                    @foreach($availableActivities as $activity)
                         <tr>
                             <td>
                                 <a href="{{ route('activity.show', $activity->id) }}">{{ $activity->title }}</a>
@@ -84,7 +47,51 @@
                     </tbody>
                 </table>
             </div>
-            @else
-                <p class="alert alert-info">Op dit moment zijn er geen activiteiten onbeschikbaar, kijk op een later moment nog eens!</p>
+        @endif
+
+        @if($upcomingActivities->count())
+            <h3>Toekomstige activiteiten</h3>
+            <!-- Unavailable activities -->
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
+                    <tr>
+                        <th>Naam</th>
+                        <th>Datum en tijd</th>
+                        <th>Aanmeldperiode</th>
+                        <th>ITP-waarde</th>
+                        <th>Acties</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($upcomingActivities as $activity)
+                        <tr>
+                            <td>
+                                <a href="{{ route('activity.show', $activity->id) }}">{{ $activity->title }}</a>
+                            </td>
+                            <td>
+                                @datetime($activity->starts_at) t/m @datetime($activity->ends_at)
+                            </td>
+                            <td>
+                                @date($activity->available_from) t/m @date($activity->available_to)
+                            </td>
+                            <td>
+                                @if(isset($activity->itp_value))
+                                    {{ $activity->itp_value }}
+                                @else
+                                    Onbekend
+                                @endif
+                            </td>
+                            <td>
+                                <a href="{{ route('activity.show', $activity->id) }}" class="btn btn-primary btn-xs">Bekijken</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @endif
+    @else
+        <p class="alert alert-info">Op dit moment zijn er geen activiteiten onbeschikbaar, kijk op een later moment nog eens!</p>
     @endif
 @endsection
