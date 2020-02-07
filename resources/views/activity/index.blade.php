4d4ebbb9f16e1d88202a652ff3695bb8ad0d8c51
cassis163 <cas.aangeenbrug@gmail.com>
2020-02-07 11:59:15 +0100

    Add two seperate tables for available and unavailable activities in activity/index.blade.php

--- resources/views/activity/index.blade.php
+++ resources/views/activity/index.blade.php
@@ -6,9 +6,49 @@
     @if ($activities->count())
         <p>Dit is een overzicht van de activiteiten die georganiseerd worden door Studievereniging "Hello World".</p>
 
+        <!-- Available activities -->
         <div class="table-responsive">
             <table class="table table-bordered table-striped table-hover">
                 <thead>
+                <tr>
+                    <th>Naam</th>
+                    <th>Datum en tijd</th>
+                    <th>Aanmeldperiode</th>
+                    <th>ITP-waarde</th>
+                    <th>Acties</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($availableActivities as $activity)
+                    <tr>
+                        <td>
+                            <a href="{{ route('activity.show', $activity->id) }}">{{ $activity->title }}</a>
+                        </td>
+                        <td>
+                            @datetime($activity->starts_at) t/m @datetime($activity->ends_at)
+                        </td>
+                        <td>
+                            @date($activity->available_from) t/m @date($activity->available_to)
+                        </td>
+                        <td>
+                            @if(isset($activity->itp_value))
+                                {{ $activity->itp_value }}
+                            @else
+                                Onbekend
+                            @endif
+                        </td>
+                        <td>
+                            <a href="{{ route('activity.show', $activity->id) }}" class="btn btn-primary btn-xs">Bekijken</a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+
+            <!-- Unavailable activities -->
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
                     <tr>
                         <th>Naam</th>
                         <th>Datum en tijd</th>
@@ -16,9 +56,9 @@
                         <th>ITP-waarde</th>
                         <th>Acties</th>
                     </tr>
-                </thead>
-                <tbody>
-                    @foreach($activities as $activity)
+                    </thead>
+                    <tbody>
+                    @foreach($unavailableActivities as $activity)
                         <tr>
                             <td>
                                 <a href="{{ route('activity.show', $activity->id) }}">{{ $activity->title }}</a>
@@ -41,10 +81,10 @@
                             </td>
                         </tr>
                     @endforeach
-                </tbody>
-            </table>
-        </div>
-    @else
-        <p class="alert alert-info">Op dit moment zijn er geen activiteiten beschikbaar, kijk op een later moment nog eens!</p>
+                    </tbody>
+                </table>
+            </div>
+            @else
+                <p class="alert alert-info">Op dit moment zijn er geen activiteiten onbeschikbaar, kijk op een later moment nog eens!</p>
     @endif
 @endsection
